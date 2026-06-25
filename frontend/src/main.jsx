f6fca9756f2a35ee755d34a83456adc6451c64d2
Diksha Aggarwal <dikshaaggarwal.in08@gmail.com>
2026-06-25 19:09:44 +0200

    Add missing import for style.css in main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { RouterProvider, createRouter } from '@tanstack/react-router'
 import { routeTree } from './routeTree.gen'
 import './index.css'
+import './style.css'
 
 // Create a new router instance
 const router = createRouter({ routeTree })
